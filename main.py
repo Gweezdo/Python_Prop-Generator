1fb4685cfbf7e5f32220a2e2013dbd224fb07da5
Zjak Theron <zjak.theron@lindsaydynan.com.au>
2020-09-22 16:56:24 +1000

    Worked further on main logic of program

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def propGenerator():
                 "450": '',
                 "270": '',
                 "90": '',
+                "jacks": ''
             }
 
             no_of_jacks = input("Do you want a Megashor Jack on one end or both ends of the prop? /nEnter '1' for one end or '2' for both ends: ")
@@ -41,15 +42,26 @@ def propGenerator():
             for i in member_list:
 
                 res = height%i
+                print("res= " + str(res))
+                print("height= " + str(height))
+                print("i= " + str(i))
+
                 
-                if height/i > 2 and res > upper_limit:
-                    temp = math.floor(height/i)
-                    prop[i] = temp
-                    print('prop[i] = ' + str(prop[i]))
-                    height = res
-                
-                # if height/i > 1 and height/i <= 2 and res > upper_limit
-                
+                if (height/i >= 2):
+                    if res > upper_limit:
+                        temp = math.floor(height/i)
+                        prop[str(i)] = temp
+                        height = res
+                        print(i)
+                    if lower_limit < res and res < upper_limit:
+                        pass
+                elif height/i >= 1 and height/i < 2 and res > upper_limit:
+                    prop[str(i)] = 1
+                    height -= i
+                    print(i)
+                elif height/i < 1 and res > lower_limit and res < upper_limit:
+                    prop["jacks"] = no_of_jacks
+                    print(i)
                     
         else:
             flag = True  
